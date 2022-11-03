507303c6abe38936a5a33ff4ddbae35e6eccb1bd
shoxrux071 <begratefuller071@gmail.com>
2022-11-04 00:00:49 +0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author "Berdimurodov Shoxrux"
+ * @since ${DATE} ${TIME} (${DAY_NAME_FULL})
+ * ${PROJECT_NAME}/${PRODUCT_NAME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/example/configs/ApplicationContex.java
@@ -0,0 +1,9 @@
+package org.example.configs;
+
+/**
+ * @author "Berdimurodov Shoxrux"
+ * @since 03/11/22 23:59 (Thursday)
+ * quiz-app-console/IntelliJ IDEA
+ */
+public class ApplicationContex {
+}
